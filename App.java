7b726bb954201efe492032fe6d3d5b286226a4a1
FlyingPandaDev <janvogele2007@icloud.com>
2024-09-20 15:54:05 +0200

    Wrapping up.

--- App.java
+++ App.java
@@ -1,16 +1,37 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class App {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
+    ArrayList<Kau> kaugummis = new ArrayList<>();
 
-    System.out.println("Welchen Farbe soll der Kaugummi haben?");
-    String farbe = scanner.nextLine();
+    while (true) {
+      System.out.println("Willkommen zu unserem Kaugummi-Generator!");
+      System.out.println("Welche Farbe soll der Kaugummi haben?");
+      String farbe = scanner.nextLine();
 
-    System.out.println("Wie groß soll der Kaugummi sein?");
-    double durchmesser = scanner.nextDouble();
+      System.out.println("Wie groß soll der Kaugummi sein?");
+      double durchmesser = scanner.nextDouble();
 
-    Kau kau = new Kau(durchmesser, farbe);
-    kau.whoami();
+      scanner.nextLine();
+
+      Kau kau = new Kau(durchmesser, farbe);
+      kaugummis.add(kau);
+      kau.whoami();
+
+      System.out.println("Wollen Sie weitere Kaugummis hinzufuegen? (j/n)");
+      String antwort = scanner.nextLine();
+
+      if (antwort.equalsIgnoreCase("j")) {
+        App.main(args);
+      } else {
+        System.out.println("Auf Wiedersehen!");
+        scanner.close();
+        break;
+      }
+      break;
+    }
+
   }
 }
